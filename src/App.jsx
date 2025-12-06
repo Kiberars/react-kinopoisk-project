5842b5f4595cc58ab673a1b7276c7971b4b023b1
kiberars <kiberars@gmail.com>
2025-12-06 11:08:57 +0500

    доработал смену темы: закончил стиль для кнопок, устаранил ошибку с сохранением темы в localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
     src="/MoveImage/Kinopoisk_colored_logo.png" 
     alt="Кинопоиск" 
     className='h-8 w-auto'/>
-    <div>
+    <div className='px-2'>
       <input type="serach" 
       value={serachTerm} onChange={e => {setSerachTerm(e.target.value)}}
       placeholder='Поиск...'
-      className='border borader-white/15 px-2 py-1 rounded outline-0'/>
+      className='border borader-white/15 px-2 py-2 rounded outline-0'/>
     
       <button onClick={toggleTheme} 
-      className='text-sm px-3 py-1 rounded border 
+      className='text-sm px-3 py-1 rounded border  
       border-white/20  
-      bg-black hover:bg-white border-white/15 text-white hover:text-black
-      dark:bg-white dark:hover:bg-black dark:border-white/10 dark:text-black dark:hover:text-white
-      transition w-20'>
+      bg-black/40 hover:bg-black/80 border-white/15 text-black hover:text-white
+      dark:bg-white/40 dark:hover:bg-white/80 dark:border-white/10 dark:text-white dark:hover:text-black
+      transition w-20 ml-2'>
         {theme === 'dark'? 'light':'dark'}
 
       </button>
--- src/components/ThemProvider.jsx
+++ src/components/ThemProvider.jsx
@@ -3,15 +3,18 @@ import { ThemeContext } from "./themeContext";
 
 
 export function ThemProvider({children}){
-    const [theme,setTheme] = useState(()=> localStorage.getItem('them')|| 'dark');
+    const [theme,setTheme] = useState(()=> localStorage.getItem('theme') || 'dark');
 
     useEffect(() => {
         document.documentElement.classList.toggle('dark',theme === 'dark')
         localStorage.setItem('theme',theme)
+        console.log('effect ', theme)
     },[theme])
 
     const toggleTheme = () =>setTheme(prev =>
         (prev === 'dark'?'light':'dark'))
+        localStorage.setItem('theme',theme)
+        console.log('toggleTheme ', theme)
 
     return (
         <ThemeContext.Provider
