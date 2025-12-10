1e72e0a70b79a6c4ca3f054bff05a3fd0296c6c4
kiberars <kiberars@gmail.com>
2025-12-10 11:16:46 +0500

    Доработал окно MovieDetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import MOVIES from '.././data/dataMoves.json';
+import { ModalTrailer } from './UI/ModalTrailer';
 
 export function MovieDetails() {
   const { id } = useParams();
@@ -8,13 +9,36 @@ export function MovieDetails() {
     return MOVIES.MOVIES.find((movie) => movie.trailerVK === id);
   }, [id]);
 
-  if (!movie) return <p>Фильмы не найдены!</p>;
+  if (!movie) return <p className='text-center mt-10 text-gray-400'>Фильмы не найдены!</p>;
 
   return (
-    <div>
-      <img src={movie.img} alt="Movie Poster" className="w-full h-auto object-cover" />
-      <h1>Карточка фильма #{id}</h1>
-      <h2>{movie.title}</h2>
+    <div className='min-h-screen px-6 py-10 bg-(--white-bg) dark:bg-(--black-bg) text-black dark:text-white'>
+      <div className='flex flex-col md:flex-row gap-10 items-start'>
+        <img src={movie.img} alt={movie.title} className="w-2/3 md:w-1/3 rounded-xl shadow-lg object-cover" />
+        <div className='flex-1 space-y-4'>
+          <h1 className='text-4xl font-bold'>{movie.title}</h1>
+          <div className='flex md:flex-row gap-4'>
+          <p className='text-xl flex-col'>Рейтинг: {movie.rating}</p>
+          {movie.rating>8 ? <p className='text-xl '>⭐⭐⭐⭐⭐</p>                  
+          : 
+          movie.rating>6 ?
+          <p className='text-xl '>⭐⭐⭐⭐</p>    
+          :
+          movie.rating>4 ?
+          <p className='text-xl '>⭐⭐⭐</p>    
+          :
+          movie.rating>2 ?
+          <p className='text-xl '>⭐⭐</p>    
+          :         
+          <p className='text-xl '>⭐</p>              
+         }
+          </div>
+          <div className='text-sm'>
+            <p>{movie.description}</p>
+          </div>
+        </div>
+       
+      </div>     
     </div>
   );
 }
